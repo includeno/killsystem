662c6e5cb10e261977b98a0233a4e72fcebc0f35
includeno <includeno@126.com>
2021-07-22 00:12:05 +0800

    添加consul服务注册与发现 完善app运行脚本

--- payment/src/main/java/com/killsystem/PaymentMain.java
+++ payment/src/main/java/com/killsystem/PaymentMain.java
@@ -2,13 +2,14 @@ package com.killsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author includeno
  * @since 2021/3/23 11:02
  */
 @SpringBootApplication
-//@EnableDiscoveryClient//zookeeper
+@EnableDiscoveryClient//zookeeper
 public class PaymentMain {
 
     public static void main(String[] args) {
