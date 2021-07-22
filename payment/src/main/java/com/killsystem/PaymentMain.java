fc46afe2f0793d43db3938fca5a0a84a635e9ffa
includeno <includeno@126.com>
2021-07-22 14:28:07 +0800

    修改springboot在docker内默认时区

--- payment/src/main/java/com/killsystem/PaymentMain.java
+++ payment/src/main/java/com/killsystem/PaymentMain.java
@@ -1,5 +1,10 @@
 package com.killsystem;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,6 +19,13 @@ public class PaymentMain {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        // 设置用户时区为 UTC
+        //TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Shanghai")));
     }
 
 }
